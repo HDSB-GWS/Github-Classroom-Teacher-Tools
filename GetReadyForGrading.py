55e8fb7661a6f4382e086da69bc4bc2bcff122a6
Chris Brooks-Prenger <brooksprench@hdsb.ca>
2022-06-19 09:46:16 -0400

    Update GetReadyForGrading.py
    Adding the ability to check a series of assignments and update all at once.

--- GetReadyForGrading.py
+++ GetReadyForGrading.py
@@ -7,13 +7,16 @@
 
 # NOTE: Requires SSH key paired with GitHub Account
 
+# TODO: Add some error handling that catches any crashes due to merge conflics and prints out a report of them instead of stopping the entire process
 # TODO: Add an argument that bypasses the entire roster and just checks a single git account. (are git accounts case sensitive?)
+
 
 import os
 import sys
 import subprocess
 
-isQuiet = False      #This script really spams the console so os.system commands replaced with subprocess and can be muted via isQuiet
+isQuiet = True      #This script really spams the console so os.system commands replaced with subprocess and can be muted via isQuiet
+checkAll = True    #Check all the assignment of a class if True
 
 def noisy(noiseIn): #Only allow console output for noisy commands if not isQuiet
     if not isQuiet:
@@ -26,6 +29,8 @@ rosterPath = r'C:\SOMEPATH\ics2oq2.txt'    #Path to your class roster (one githu
 #Assignment Info
 assignmentName = 'example-repo'            #The repo assignment name in github classroom
 repoPath = r'C:\SOMEPATH\ExampleRepo'      #The path to save the repos that are downloaded
+assignmentList = []
+#     assignmentList.append([assignmentName,repoPath])
 
 
 #----------------------------------------------------------------------------------------------
@@ -45,85 +50,106 @@ def feedbackBranchCheckAndCheckout():
         noisy(subprocess.check_output('git checkout feedback' , shell=True)) #Checkout branch
         
 startingPath = os.getcwd()
-summary = []
+
 
 with open(rosterPath) as f:
     names = f.read().splitlines()
 
-for name in names:
-    print(name)
-    
-    folderName = f'{assignmentName}-{name}'
-    
+summaryList = []
+assignmentList.reverse()
 
-    #Find if default branch is master or main
-    defaultBranch = ""
+for assignment in assignmentList:
+    assignmentName = assignment[0]
+    repoPath = assignment[1]
+    summary = []
     
-    checkBranchCmd = f'git ls-remote --heads git@github.com:{orgName}/{folderName} '
-    
-    try:
-        if (len(subprocess.check_output(checkBranchCmd + "main", shell=True)) > 0):
-            defaultBranch = 'main'
-        elif (len(subprocess.check_output(checkBranchCmd + "master", shell=True)) > 0):
-            defaultBranch = 'master'
-        else:
-            print("Whoa, something is really wrong.  Go see why there isn't a main/master branch")
-            break
-    except subprocess.CalledProcessError as e:
-        if e.returncode == 128: #Repo does not exist, or incorrect permissions
-            print( f'Error: {orgName}/{folderName} does not exist')
-            summary.append(f'{name} : Repo does not exist')
-            continue            #Skip everything else!
-        else:
-            raise e    
-            
+    for name in names:
+        print(name)
         
-    print(f'{repoPath}\\{folderName}')
-    if os.path.exists(f'{repoPath}\\{folderName}'):   #Check to see if the project has been cloned before
+        folderName = f'{assignmentName}-{name}'
         
-        os.chdir(f'{repoPath}/{folderName}')          #Set correct repo path
-        
-        #os.system('git stash -u')
-        #print("pre")
-        feedbackBranchCheckAndCheckout()
 
-        noisy(subprocess.check_output('git pull' , shell=True))                   #Pull any changes from feedback branch
-        #print("post")
-        noisy(subprocess.check_output('git stash -u', shell=True))                #Stash current changes
-        noisy(subprocess.check_output('git checkout '+defaultBranch , shell=True))#Switch to default (aka student) branch
-        noisy(subprocess.check_output('git pull' , shell=True))                   #Pull any changes from default branch
-        #feedbackBranchCheckAndCheckout()
-        noisy(subprocess.check_output('git checkout feedback' , shell=True))      #Switch back to feedback branch
-        noisy(subprocess.check_output('git merge '+defaultBranch , shell=True))   #Merge in any changes made by student in default branch
-        noisy(subprocess.check_output('git push' , shell=True))                   #Push the changes into the remote server
+        #Find if default branch is master or main
+        defaultBranch = ""
+        
+        checkBranchCmd = f'git ls-remote --heads git@github.com:{orgName}/{folderName} '
+        
         try:
-            noisy(subprocess.check_output('git stash apply' , shell=True))            #Reapply all stashed changes
-        except subprocess.CalledProcessError as e:  #If there is no stash to apply, this returns an error code of 1
-            if e.returncode != 1:                   #So catch the error unless it's not 1, then let it through
-                raise e                             #TODO: Add a check to only reapply the stash if files were stashed
-
-        lastCommitDateCmd = 'git log -1 --format=%cd' #Get the date and time of most recent commit
-        lastCommitDate = subprocess.check_output(lastCommitDateCmd, shell=True);  #Save the result for use later
-        print(f'Last Commit by {name} : {lastCommitDate.decode("utf-8")}')
-        summary.append(f'{name} : {lastCommitDate.decode("utf-8")}')
-        
-        
-    else:     #Project has not been cloned before, so grab it.
-                                                      #Clone Repo
-        cloneCmd = f'git clone git@github.com:{orgName}/{folderName} {repoPath}\\{folderName}'
-        print(cloneCmd)
-        os.system(cloneCmd)
+            if (len(subprocess.check_output(checkBranchCmd + "main", shell=True)) > 0):
+                defaultBranch = 'main'
+            elif (len(subprocess.check_output(checkBranchCmd + "master", shell=True)) > 0):
+                defaultBranch = 'master'
+            else:
+                print("Whoa, something is really wrong.  Go see why there isn't a main/master branch")
+                break
+        except subprocess.CalledProcessError as e:
+            if e.returncode == 128: #Repo does not exist, or incorrect permissions
+                print( f'Error: {orgName}/{folderName} does not exist')
+                summary.append(f'{name} : Repo does not exist')
+                continue            #Skip everything else!
+            else:
+                raise e    
+                
             
-        os.chdir(f'{repoPath}/{folderName}')         #Set correct repo path
-        
-        feedbackBranchCheckAndCheckout()
+        print(f'{repoPath}\\{folderName}')
+        if os.path.exists(f'{repoPath}\\{folderName}'):   #Check to see if the project has been cloned before
+            
+            os.chdir(f'{repoPath}/{folderName}')          #Set correct repo path
+            
+            #os.system('git stash -u')
+            #print("pre")
+            feedbackBranchCheckAndCheckout()
 
-        
-    os.chdir(startingPath)                        #Reset the path back to the starting folder
+            noisy(subprocess.check_output('git pull' , shell=True))                   #Pull any changes from feedback branch
+            #print("post")
+            noisy(subprocess.check_output('git stash -u', shell=True))                #Stash current changes
+            noisy(subprocess.check_output('git checkout '+defaultBranch , shell=True))#Switch to default (aka student) branch
+            noisy(subprocess.check_output('git pull' , shell=True))                   #Pull any changes from default branch
+            #feedbackBranchCheckAndCheckout()
+            noisy(subprocess.check_output('git checkout feedback' , shell=True))      #Switch back to feedback branch
+            noisy(subprocess.check_output('git merge '+defaultBranch , shell=True))   #Merge in any changes made by student in default branch
+            noisy(subprocess.check_output('git push' , shell=True))                   #Push the changes into the remote server
+            try:
+                noisy(subprocess.check_output('git stash apply' , shell=True))            #Reapply all stashed changes
+            except subprocess.CalledProcessError as e:  #If there is no stash to apply, this returns an error code of 1
+                if e.returncode != 1:                   #So catch the error unless it's not 1, then let it through
+                    raise e                             #TODO: Add a check to only reapply the stash if files were stashed
 
-print("Summary")
-summary = sorted(summary, key=str.casefold)
-for entry in summary:
-    print(entry.strip())
+            lastCommitDateCmd = 'git log -1 --format=%cd' #Get the date and time of most recent commit
+            lastCommitDate = subprocess.check_output(lastCommitDateCmd, shell=True);  #Save the result for use later
+            print(f'Last Commit by {name} : {lastCommitDate.decode("utf-8")}')
+            summary.append(f'{name} : {lastCommitDate.decode("utf-8")}')
+            
+            
+        else:     #Project has not been cloned before, so grab it.
+                                                          #Clone Repo
+            cloneCmd = f'git clone git@github.com:{orgName}/{folderName} {repoPath}\\{folderName}'
+            print(cloneCmd)
+            os.system(cloneCmd)
+                
+            os.chdir(f'{repoPath}/{folderName}')         #Set correct repo path
+            
+            feedbackBranchCheckAndCheckout()
+            summary.append(f'{name} : Repo Cloned (and Feedback branch created if necessary)')
 
+            
+        os.chdir(startingPath)                        #Reset the path back to the starting folder
 
+     
+    #print("Summary")
+    
+    summary = sorted(summary, key=str.casefold)
+    summary.insert(0, f'{assignmentName}')
+    summaryList.append(summary)
+#     for entry in summary:
+#         print(entry.strip())
+#     
+    
+    if not checkAll:
+        break
+
+summaryList.reverse()
+for summary in summaryList:
+    for entry in summary:
+        print(entry.strip())
+    print()
